2e59fc63032c80a4674e118feed2b987156215ae
Adam Wiles <adam.wiles@newscred.com>
2021-09-26 10:07:55 -0400

    Add resizing for more image formats

--- src/main.rs
+++ src/main.rs
@@ -56,9 +56,9 @@ mod storage {
                 .collect()
         }
 
-        pub async fn create(&self, content: &[u8], image_name: String, width: Option<u32>, height: Option<u32>) -> u16 {
+        pub async fn create(&self, content: &[u8], image_name: &str, width: Option<u32>, height: Option<u32>) -> u16 {
             let mut folder = IMAGE_FOLDER;
-            let mut name = image_name;
+            let mut name = format!("{}", image_name);
             if width.is_some() || height.is_some() {
                 folder = CACHE_FOLDER;
                 name = format!("{}_{}_{}", name, width.unwrap_or(0), height.unwrap_or(0));
@@ -85,13 +85,112 @@ mod storage {
     }
 }
 
+#[derive(Clone)]
+struct ImageService {
+    storage: storage::Storage
+}
+
+impl ImageService {
+    pub fn new() -> ImageService {
+        let storage = storage::Storage::new();
+        ImageService {
+            storage
+        }
+    }
+
+    fn get_image_output_format(file_name: &str) -> Option<ImageOutputFormat> {
+        let name_parts: Vec<&str> = file_name.split(".").collect();
+        if name_parts.len() == 2 {
+            match name_parts[1] {
+                "jpeg" => Some(ImageOutputFormat::Jpeg(75)),
+                "jpg" => Some(ImageOutputFormat::Jpeg(75)),
+                "png" => Some(ImageOutputFormat::Png),
+                "ico" => Some(ImageOutputFormat::Ico),
+                "gif" => Some(ImageOutputFormat::Gif),
+                _ => None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn get_image_format(file_name: &str) -> Option<ImageFormat> {
+        let name_parts: Vec<&str> = file_name.split(".").collect();
+        if name_parts.len() == 2 {
+            match name_parts[1] {
+                "jpeg" => Some(ImageFormat::Jpeg),
+                "jpg" => Some(ImageFormat::Jpeg),
+                "png" => Some(ImageFormat::Png),
+                "ico" => Some(ImageFormat::Ico),
+                "gif" => Some(ImageFormat::Gif),
+                _ => None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn get_content_header(format: &ImageFormat) -> &'static str {
+        match format {
+            ImageFormat::Jpeg => "image/jpeg",
+            ImageFormat::Png => "image/png",
+            ImageFormat::Ico => "image/ico",
+            ImageFormat::Gif => "image/gif",
+            _ => "image/jpeg"
+        }
+    }
+
+    async fn get_image(&self, image_name: &str, width: Option<u32>, height: Option<u32>) -> Option<(Vec<u8>, &'static str)> {
+        let image_format = ImageService::get_image_format(&image_name).unwrap();
+        let image_format_header = ImageService::get_content_header(&image_format);
+
+        let image = self.storage.get(image_name.to_string(), width, height).await;
+        if image.is_some() {
+            Some((image.unwrap(), image_format_header))
+        } else if width.is_none() && height.is_none() {
+            None
+        } else {
+            let original_image = self.storage.get(image_name.to_string(), None, None).await;
+            if original_image.is_none() {
+                None
+            } else {
+                let img = image::load_from_memory_with_format(&original_image.unwrap(), image_format).unwrap_or_else(|err| {
+                    panic!("failed to load image {}", err)
+                });
+
+                let image_output_format = ImageService::get_image_output_format(&image_name);
+                if image_output_format.is_none() {
+                    return None
+                }
+                let mut w: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+                let resized_image = img.resize_exact(width.unwrap(), height.unwrap(), FilterType::Nearest);
+                resized_image.write_to(&mut w, image_output_format.unwrap()).unwrap();
+                let image_bytes = w.into_inner();
+                self.storage.create(&image_bytes, &image_name, width, height).await;
+                Some((image_bytes, image_format_header))
+            }
+        }
+    }
+
+    async fn list(&self) -> Vec<String> {
+        self.storage.list().await
+    }
+
+    async fn create(&self, content: &[u8], content_type: &str) -> (String, u16) {
+        let mime_type: Vec<&str> = content_type.split("/").collect();
+        let image_name = format!("{}.{}", Uuid::new_v4(), mime_type.last().unwrap());
+        let code = self.storage.create(content, &image_name, None, None).await;
+        (image_name, code)
+    }
+}
+
 #[derive(Serialize)]
 struct ListResponse {
     files: Vec<String>
 }
 
 async fn handle_image_list(context: web::Data<ServerContext>) -> Result<HttpResponse, Error> {
-    let files = context.storage.list().await;
+    let files = context.image_service.list().await;
     Ok(HttpResponse::Ok().json(ListResponse { files }))
 }
 
@@ -104,9 +203,8 @@ async fn handle_image_upload(req: HttpRequest, body: web::Bytes, context: web::D
     let content_type_header = req.headers().get("Content-Type").unwrap_or_else(|| {
         panic!("No Content Type")
     });
-    let mime_type: Vec<&str> = content_type_header.to_str().unwrap().split("/").collect();
-    let filename = format!("{}.{}", Uuid::new_v4(), mime_type.last().unwrap());
-    let code = context.storage.create(&body, filename.to_string(), None, None).await;
+    let content_type = content_type_header.to_str().unwrap();
+    let (filename, code) = context.image_service.create(&body, &content_type).await;
     let resp = UploadResponse { id: filename };
     match code {
         200 => Ok(HttpResponse::Ok().json(resp)),
@@ -121,45 +219,26 @@ struct GetImageParams {
     h: Option<u32>,
 }
 
-async fn handle_image_get(image_id: web::Path<String>, params: web::Query<GetImageParams>, context: web::Data<ServerContext>) -> Result<HttpResponse, Error> {
-    let image = context.storage.get(image_id.to_string(), params.w, params.h).await;
-    if image.is_some() {
-        Ok(HttpResponse::Ok()
-                .content_type("image/jpeg")
-                .body(image.unwrap()))
-    } else if params.w.is_none() && params.h.is_none() {
-        Err(error::ErrorNotFound("no image found"))
-    } else {
-        let original_image = context.storage.get(image_id.to_string(), None, None).await;
-        if original_image.is_none() {
-            Err(error::ErrorNotFound("no image found"))
-        } else {
-            let img = image::load_from_memory_with_format(&original_image.unwrap(), ImageFormat::Jpeg).unwrap_or_else(|err| {
-                panic!("failed to load image {}", err)
-            });
-            let mut w: Cursor<Vec<u8>> = Cursor::new(Vec::new());
-            let resized_image = img.resize_exact(params.w.unwrap(), params.h.unwrap(), FilterType::Nearest);
-            resized_image.write_to(&mut w, ImageOutputFormat::Jpeg(75)).unwrap();
-            let image_bytes = w.into_inner();
-            context.storage.create(&image_bytes, image_id.to_string(), params.w, params.h).await;
+async fn handle_image_get(image_name: web::Path<String>, params: web::Query<GetImageParams>, context: web::Data<ServerContext>) -> Result<HttpResponse, Error> {
+    let image = context.image_service.get_image(&image_name, params.w, params.h).await;
+    match image {
+        Some((image_data, content_type_header)) =>
             Ok(HttpResponse::Ok()
-                .content_type("image/jpeg")
-                .body(image_bytes))
-        }
+                .content_type(content_type_header)
+                .body(image_data)),
+        None => Err(error::ErrorNotFound("no image found")),
     }
 }
 
-use storage::Storage;
-
 #[derive(Clone)]
 struct ServerContext {
-    storage: Storage
+    image_service: ImageService
 }
 
 impl ServerContext {
     fn new() -> ServerContext {
         ServerContext {
-            storage: Storage::new(),
+            image_service: ImageService::new(),
         }
     }
 }
